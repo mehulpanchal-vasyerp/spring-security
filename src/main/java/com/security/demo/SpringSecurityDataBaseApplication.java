9062b8156e9c11bdc305b42d87b283345fe4af0e
Mehul Panchal <mehulpanchal@vasyerp.com>
2024-02-20 18:08:50 +0530

    implement database authentication

---
+++ src/main/java/com/security/demo/SpringSecurityDataBaseApplication.java
@@ -0,0 +1,13 @@
+package com.security.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDataBaseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/security/demo/dto/UserDetailDto.java
@@ -0,0 +1,12 @@
+package com.security.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDetailDto {
+	 private String name;
+	    private String email;
+		private String username;
+		private String password;
+		private String role;
+}
